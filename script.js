1647f68d035935c7fde471c9704a15c6f20de195
Leon Tzu Chiang Chan <leontzuchiangchan@Leons-MacBook-Air.local>
2022-12-19 15:29:24 -0500

    game begins

--- script.js
+++ script.js
@@ -6,8 +6,43 @@ class Daimyo {
   }
 }
 
-let samurai1 = new Daimyo("Nobunaga", "Trident");
+let samurai1 = new Daimyo("Nobunaga", "Sword");
 
-let samurai2 = new Daimyo("Hideyoshi", "Club");
+let samurai2 = new Daimyo("Hideyoshi", "Naginata");
 
-let samurai3 = new Daimyo("Ieyasu", "Spear");
+let samurai3 = new Daimyo("Ieyasu", "Katanas");
+
+class battle {
+  constructor(name) {
+    this.name = name;
+  }
+  samurais = [];
+  add(warrior) {
+    this.samurais.push(warrior);
+  }
+  fight() {
+    if (
+      this.samurais[0].weapon === 'Sword' &&
+      this.samurais[1].weapon === 'Katanas'
+    ) {
+      console.log(this.samurais[0].name, 'wins')
+    }
+    if (
+      this.samurais[0].weapon === 'Katanas' &&
+      this.samurais[1].weapon === 'Naginata'
+    ) {
+      console.log(this.samurais[0].name, 'wins')
+    }
+  }
+}
+
+const battlefield = new battle("dojo");
+
+battlefield.add(samurai1);
+console.log(battlefield);
+
+battlefield.add(samurai2);
+console.log(battlefield);
+
+battlefield.add(samurai3);
+console.log(battlefield);
