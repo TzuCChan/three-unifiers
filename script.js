bcedd35250dc768790fad40f22fd272d42caab52
Leon Tzu Chiang Chan <leontzuchiangchan@Leons-MacBook-Air.local>
2022-12-19 13:43:42 -0500

    add characters & weapons

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("script loaded");
+class Daimyo {
+  constructor(name, weapon) {
+    this.name = name;
+    this.weapon = weapon;
+  }
+}
+
+let samurai1 = new Daimyo("Nobunaga", "Trident");
+
+let samurai2 = new Daimyo("Hideyoshi", "Club");
+
+let samurai3 = new Daimyo("Ieyasu", "Spear");
